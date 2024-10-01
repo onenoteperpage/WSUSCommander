f5440036ee7fe4b7ed820cff5f52800a927d15c8
OneNotePerPage <danijel.james+onenoteperpage@gmail.com>
2024-10-01 17:29:55 +1000

    update gitignore

--- build.ps1
+++
@@ -1,4 +0,0 @@
-dotnet clean;
-dotnet restore;
-dotnet publish -c Release -r win-x64 --self-contained true -p:PublishSingleFile=true;
-& C:\Users\Danijel.Wynyard\source\repos\WSUSCommander\bin\Release\net8.0-windows\win-x64\publish\WSUSCommander.exe
