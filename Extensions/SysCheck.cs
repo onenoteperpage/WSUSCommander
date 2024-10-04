8638a11a97932a38a5abd116d6c38ca2dd5dfb34
OneNotePerPage <danijel.james+onenoteperpage@gmail.com>
2024-10-04 12:08:04 +1000

    add async version

--- Extensions/SysCheck.cs
+++ Extensions/SysCheck.cs
@@ -14,7 +14,37 @@ namespace WSUSCommander.Extensions
                     PingReply reply = ping.Send(machineName, 3000); // Timeout of 3000 ms (3 seconds)
                     if (reply.Status == IPStatus.Success)
                     {
-                        CLogger.Info($"Pint to '{machineName}' successful.");
+                        CLogger.Info($"Ping to '{machineName}' successful.");
+                        return true;
+                    }
+                }
+            }
+            catch (PingException)
+            {
+                // Ping failed due to network issues or invalid machine name
+                CLogger.Debug($"Ping to '{machineName}' failed due to network issues or invalid machine name");
+                return false;
+            }
+            catch (Exception ex)
+            {
+                CLogger.Error($"An error occurred: {ex.Message}");
+                return false;
+            }
+
+            CLogger.Warn($"Unknown 'false' for ping to '{machineName}'.");
+            return false;
+        }
+
+        public static async Task<bool> IsMachineReachableAsync(string machineName)
+        {
+            try
+            {
+                using (Ping ping = new Ping())
+                {
+                    PingReply reply = await ping.SendPingAsync(machineName, 3000); // Timeout of 3000 ms (3 seconds)
+                    if (reply.Status == IPStatus.Success)
+                    {
+                        CLogger.Info($"Ping to '{machineName}' successful.");
                         return true;
                     }
                 }
